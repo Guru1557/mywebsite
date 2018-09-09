013a44951f25d9a72c561002568838ab1c468910
Gurpreet <gurpreetrandhawa002@gmail.com>
2018-09-09 09:25:31 -0700

    Admin and user login

---
+++ application/modules/about/views/index.php
@@ -0,0 +1,25 @@
+<!-- page content -->
+ <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper settingPage">
+    <!-- Main content -->
+    <section class="content">
+      <!-- Default box -->
+      <div class="box box-success">
+        <div class="box-header with-border">
+          <h3 class="box-title">About Us </h3>
+        </div>
+        <div class="box-body">
+          <div class="row">
+            <div class="col-lg-12">
+            
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
---
+++ application/modules/user/views/login.php
@@ -0,0 +1,47 @@
+<body  style="background: -webkit-linear-gradient(left, rgba(68, 174, 228, 0.5), rgba(159, 65, 194, 0.5));">
+<div style="">
+	<div class="login-box" >
+	  	<div class="login-logo">
+	    	<a href="<?php echo base_url(); ?>"><b>Pen and Paper</b></a>
+	  	</div>
+	  	<!-- /.login-logo -->
+	  	<div style="  background:#fae6e6;
+    padding: 1%;
+	 border-radius: 15px;
+	 margin-top:3%;
+	 margin-bottom:3%;
+	 box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 
+	 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+	  0px 2px 1px -1px rgba(0, 0, 0, 0.12);">
+	    	<p class="login-box-msg">Sign in to start your session</p>
+			<?php if($this->session->flashdata("messagePr")){?>
+	  		<div class="alert alert-info">      
+		        <?php echo $this->session->flashdata("messagePr")?>
+		    </div>
+		    <?php } ?>
+		    <form action="<?php echo base_url().'user/auth_user'; ?>" method="post">
+		    	<div class="form-group has-feedback">
+		    		<input type="text" name="email" class="form-control" id="" placeholder="Email">
+		        	<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+		      	</div>
+				<div class="form-group has-feedback">
+					<input type="password" name="password" class="form-control" id="pwd" placeholder="Password" >
+					<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+				</div>
+			    <a href="forgetpassword" class="forgot ">I Forgot my password?</a>
+			  	<div class="row">
+			  		<div class="col-xs-12">
+		          		<button type="submit" class="btn btn-primary btn-block btn-flat btn-color">Sign In</button>
+		        	</div>
+				</div>
+		    </form>
+		    <!-- /.social-auth-links -->
+		    <?php if(setting_all('register_allowed')==1){ ?>
+		    	<br>
+				<span class="glyphicon glyphicon-user bg-icon-paste"></span><a href="<?php echo base_url().'user/registration';?>" class="text-center"> Register a new membership</a>
+			<?php } ?>
+		</div>
+		<!-- /.login-box-body -->
+	</div>
+			</div>
+</body>
